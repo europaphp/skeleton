99bb9ce03b1d264b4679205da0365640c46240eb
Trey Shugart <tres@trey-shugarts-macbook.local>
2010-03-08 22:40:19 +1100

    Update doc blocks for example IndexController.

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,10 +1,24 @@
 <?php
 
+/**
+ * @author Trey Shugart
+ */
+
+/**
+ * An example controller sketching some of the features that can be implemented.
+ * 
+ * Europa_Controller_Abstract is an optional base class, but provides a few
+ * default properties and methods to make your life a bit easier.
+ * 
+ * @package IndexController
+ */
 class IndexController extends Europa_Controller_Abstract
 {
 	/**
 	 * The constructor. Similar to init, but init allows a return value
 	 * besides the controller instance.
+	 * 
+	 * @return IndexController
 	 */
 	public function __construct()
 	{
@@ -15,6 +29,8 @@ class IndexController extends Europa_Controller_Abstract
 	 * Traps any calls to an undefined method. This will get called after
 	 * IndexController->init().
 	 * 
+	 * @param string $name
+	 * @param array $args
 	 * @return void
 	 */
 	public function __call($name, $args)
@@ -43,6 +59,15 @@ class IndexController extends Europa_Controller_Abstract
 	 * in an array are applied to the view. If false is returned, the view
 	 * is then disabled and not rendered.
 	 * 
+	 * Parameters passed to actions are retrieved using
+	 * Europa_Route->getParam() and are casted according to type. For example,
+	 * index.php?controller=index&action=index&msg=test would make $msg
+	 * 'test' instead of 'Welcome to EuropaPHP!'. 
+	 * 
+	 * If a parameter is required and isn't passed either via the route, $_GET
+	 * or $_POST, then an exception will be thrown notifying as much.
+	 * 
+	 * @param string $msg
 	 * @return array|false|void
 	 */
 	public function indexAction($msg = 'Welcome to EuropaPHP!')
