df092ff6fbb4f7419724c848a026f29efe9a0566
Trey Shugart <tres@trey-shugarts-macbook.local>
2010-05-05 00:32:54 +1000

    Remove controller not found exception since a reflection exception will be thrown anyways.

--- europa/lib/Europa/Request/Exception.php
+++ europa/lib/Europa/Request/Exception.php
@@ -12,20 +12,14 @@
 class Europa_Request_Exception extends Europa_Exception
 {
 	/**
-	 * The exception/error code that identifies and exception with a
-	 * controller not being found.
-	 */
-	const CONTROLLER_NOT_FOUND = 1;
-	
-	/**
 	 * The exception/error code that identifies and exception with a action
 	 * not being found.
 	 */
-	const ACTION_NOT_FOUND = 2;
+	const ACTION_NOT_FOUND = 1;
 
 	/**
 	 * Fired when a required parameter inside an action is not defined in
 	 * the request.
 	 */
-	const REQUIRED_PARAMETER_NOT_DEFINED = 3;
+	const REQUIRED_PARAMETER_NOT_DEFINED = 2;
 }
