4d5b3cfa752512c0f114229cb8eb7bd9de8882e2
Trey Shugart <tres@trey-shugarts-macbook.local>
2010-05-16 18:34:49 +1000

    Modify to reflect structural changes.

--- sandbox/index.php
+++ sandbox/index.php
@@ -8,11 +8,11 @@ ini_set('display_errors', 'on');
 define('EUROPA_START_TIME', microtime());
 
 // require the loader, register autoloading and define load paths
-require 'lib/Europa/Loader.php';
+require '../europa/Europa/Loader.php';
 Europa_Loader::registerAutoload();
 Europa_Loader::addPath('./app/controllers');
 Europa_Loader::addPath('./app/helpers');
-Europa_Loader::addPath('./lib');
+Europa_Loader::addPath('../europa');
 
 // dispatch the request catching any exceptions
 try {
