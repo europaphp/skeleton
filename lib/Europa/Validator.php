a1a99ebc2ee6bd7dbfda4666d018499d0c9c3e89
unknown <Trey Shugart@.(none)>
2010-05-20 18:59:03 +1000

    Update validators.

--- lib/Europa/Validator.php
+++ lib/Europa/Validator.php
@@ -9,29 +9,8 @@
  * @license  (c) 2010 Trey Shugart
  * @link     http://europaphp.org/license
  */
-abstract class Europa_Validator
+abstract class Europa_Validator implements Europa_Validator_Validatable
 {
-	/**
-	 * The message level associated to errors.
-	 * 
-	 * @var int
-	 */
-	const LEVEL_ERROR = 3;
-	
-	/**
-	 * The message level associated to warnings.
-	 * 
-	 * @var int
-	 */
-	const LEVEL_WARNING = 2;
-	
-	/**
-	 * The message level associated to successes.
-	 * 
-	 * @var int
-	 */
-	const LEVEL_SUCCESS = 0;
-	
 	/**
 	 * The messages associated to the validator. Messages are organized into
 	 * levels. The default levels are provided as LEVEL_ constants in this
@@ -42,26 +21,15 @@ abstract class Europa_Validator
 	protected $_messages = array();
 	
 	/**
-	 * Performs validation and returns whether or not the value associated to
-	 * the validator instance is valid or not.
-	 * 
-	 * @return bool
-	 */
-	abstract public function isValid($value);
-	
-	/**
 	 * Adds a message to the validator for the indicated level.
 	 * 
 	 * @param string $msg The message to add.
 	 * @param int $level The level to add the message to.
 	 * @return Europa_Validator
 	 */
-	public function addMessage($message, $level = Europa_Validator::LEVEL_ERROR)
+	public function addErrorMessage($message)
 	{
-		if (!is_array($this->_messages[$level]) {
-			$this->_messages[$level] = array();
-		}
-		$this->_messages[$level][] = $message;
+		$this->_messages[] = $message;
 		return $this;
 	}
 	
@@ -71,11 +39,8 @@ abstract class Europa_Validator
 	 * @param int $level The error level to retrieve the messages for.
 	 * @return array
 	 */
-	public function getMessages($level = Europa_Validator::LEVEL_ERROR)
+	public function getErrorMessages()
 	{
-		if (isset$this->_messages[$level]) {
-			return $this->_messages[$level];
-		}
-		return array();
+		return $this->_messages;
 	}
 }
---
+++ lib/Europa/Validator/Suite.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * An abstract class for validator classes.
+ * 
+ * @category Validation
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  (c) 2010 Trey Shugart
+ * @link     http://europaphp.org/license
+ */
+class Europa_Validator_Suite implements Europa_Validator_Validatable
+{
+	/**
+	 * 
+	 */
+	protected $_errors = array();
+	
+	/**
+	 * 
+	 */
+	protected $_validators = array();
+	
+	/**
+	 * 
+	 */
+	public function validate(array $data)
+	{
+		$valid = true;
+		foreach ($this->_validators as $name => $group) {
+			$value = null;
+			if (isset($data[$name])) {
+				$value = $data[$name];
+			}
+			foreach ($group as $validator) {
+				if (!$validator->validate($value)) {
+					$valid = false;
+					$this->_errors[] = $validator;
+				}
+			}
+		}
+		return $valid;
+	}
+	
+	/**
+	 * 
+	 */
+	public function addValidator($name, Europa_Validator_Validatable $validator)
+	{
+		if (!isset($this->_validators[$name])) {
+			$this->_validators[$name] = array();
+		}
+		$this->_validators[$name][] = $validator;
+		return $this;
+	}
+	
+	/**
+	 * 
+	 */
+	public function getErrors()
+	{
+		return $this->_errors;
+	}
+	
+	/**
+	 * 
+	 */
+	public function getErrorMessages()
+	{
+		$messages = array();
+		foreach ($this->getErrors() as $error) {
+			foreach ($error->getErrorMessages() as $message) {
+				$messages[] = $message;
+			}
+		}
+		return $messages;
+	}
+}
