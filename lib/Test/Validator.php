f64882f754a402873cf9328ad8d157be2420ab72
Trey Shugart <treshugart@gmail.com>
2010-09-01 14:50:12 +1000

    Update docblocks.

--- lib/Test/Validator.php
+++ lib/Test/Validator.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Europa_Validator test suite.
+ * 
+ * @category Tests
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  (c) 2010 Trey Shugart
+ * @link     http://europaphp.org/license
+ */
 class Test_Validator extends Europa_Unit_Suite
 {
 	
