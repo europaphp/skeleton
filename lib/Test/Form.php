4f3fccd5bfe54a3a3d4fc22c30a0c85d9a4f7806
Trey Shugart <treshugart@gmail.com>
2010-09-01 14:50:59 +1000

    Update tests for Europa_Form.

--- lib/Test/Form.php
+++ lib/Test/Form.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Europa_Form test suite.
+ * 
+ * @category Tests
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  (c) 2010 Trey Shugart
+ * @link     http://europaphp.org/license
+ */
 class Test_Form extends Europa_Unit_Suite
 {
 	 
--- lib/Test/Form/ElementList.php
+++ lib/Test/Form/ElementList.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Tests for validating Europa_Form_ElementList.
+ * 
+ * @category Tests
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  (c) 2010 Trey Shugart
+ * @link     http://europaphp.org/license
+ */
 class Test_Form_ElementList extends Europa_Unit_Test
 {
 	/**
@@ -25,15 +34,38 @@ class Test_Form_ElementList extends Europa_Unit_Test
 	 * 
 	 * @return bool
 	 */
-	public function testAddition()
+	public function testElementExistence()
 	{
 		if (!$this->_list['name'] instanceof Europa_Form_Element_Input) {
 			return false;
 		}
 		return true;
 	}
+	
+	/**
+	 * Tests validation on all elements in the list.
+	 * 
+	 * @return bool
+	 */
+	public function testValidation()
+	{
+		$required = new Europa_Validator_Required;
+		$required->addMessage('Name is required.');
+		$this->_list['name']->setValidator($required)->value = 'not empty so it can be valid';
+		
+		return $this->_list->validate()->isValid();
+	}
 }
 
+/**
+ * Dummy class for validating the Europa_Form_ElementList
+ * 
+ * @category Tests
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  (c) 2010 Trey Shugart
+ * @link     http://europaphp.org/license
+ */
 class Test_Form_TestElementList extends Europa_Form_ElementList
 {
 	public function __toString()
