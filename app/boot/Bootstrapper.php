52719e88aa9da108e981cd62f4b65488b26226c9
Trey Shugart <treshugart@gmail.com>
2010-12-12 09:50:00 +1100

    Add a app/boot directory for containing default bootstrapping procedures.

---
+++ app/boot/Bootstrapper.php
@@ -0,0 +1,75 @@
+<?php
+
+// autoloading isn't enabled yet, so required the bootstrapper
+require_once dirname(__FILE__) . '/../../lib/Europa/Bootstrapper.php';
+
+/**
+ * Bootstraps the sample application.
+ * 
+ * @category Bootstrapping
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  Copyright (c) 2010 Trey Shugart http://europaphp.org/license
+ */
+class Bootstrapper extends Europa_Bootstrapper
+{
+    /**
+     * The application base path.
+     * 
+     * @var string
+     */
+    private $_base;
+    
+    /**
+     * Sets error reporting.
+     * 
+     * @return void
+     */
+    public function setErrorReporting()
+    {
+        error_reporting(E_ALL ^ E_STRICT);
+        ini_set('display_errors', 'on');
+    }
+    
+    /**
+     * Sets the base path.
+     * 
+     * @return void
+     */
+    public function setBasePath()
+    {
+        $this->_base = realpath(dirname(__FILE__) . '/../../');
+    }
+    
+    /**
+     * Requires the loader.
+     * 
+     * @return void
+     */
+    public function requireLoader()
+    {
+        require $this->_base . '/lib/Europa/Loader.php';
+    }
+    
+    /**
+     * Adds load paths to the loader for autoloading.
+     * 
+     * @return void
+     */
+    public function addLoadPaths()
+    {
+        Europa_Loader::addPath($this->_base . '/app/controllers');
+        Europa_Loader::addPath($this->_base . '/app/views');
+        Europa_Loader::addPath($this->_base . '/app/helpers');
+    }
+    
+    /**
+     * Registers autoloading.
+     * 
+     * @return void
+     */
+    public function registerAutoloading()
+    {
+        Europa_Loader::registerAutoload();
+    }
+}
---
+++ app/boot/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+// require the bootstrapper and boot the application
+require_once dirname(__FILE__) . '/Bootstrapper.php';
+$app = new Bootstrapper;
+$app->boot();
