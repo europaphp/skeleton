9390dcfba7f6dbbca353a14831fa06c90efd4143
Trey Shugart <treshugart@gmail.com>
2010-12-12 09:54:09 +1100

    Moved bootstrapping stuff from the index to the app specific (sample) Bootstrapper class in app/boot.

--- www/index.php
+++ www/index.php
@@ -1,19 +1,10 @@
 <?php
 
-// error reporting and levels
-error_reporting(E_ALL ^ E_STRICT);
-ini_set('display_errors', 'on');
-
-// for reporting puroses
+// for showing off
 define('EUROPA_START_TIME', microtime());
 
-// require the loader, register autoloading and define load paths
-$base = dirname(__FILE__) . '/';
-require $base . '/../lib/Europa/Loader.php';
-Europa_Loader::registerAutoload();
-Europa_Loader::addPath($base . 'app/controllers');
-Europa_Loader::addPath($base . 'app/views');
-Europa_Loader::addPath($base . 'app/helpers');
+// load the bootstrap
+require dirname(__FILE__) . '/../app/boot/bootstrap.php';
 
 // dispatch the request catching any exceptions
 try {
