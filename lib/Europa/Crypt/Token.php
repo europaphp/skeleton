37cd5a1fa5dcb1a51e2444f4ad6d257ad15394aa
Trey Shugart <treshugart@gmail.com>
2010-12-12 10:03:39 +1100

    Crypt/Token docblocks.

--- lib/Europa/Crypt/Token.php
+++ lib/Europa/Crypt/Token.php
@@ -1,16 +1,48 @@
 <?php
 
+/**
+ * A class for generating and sharing an non-reversible token.
+ * 
+ * @category Encryption
+ * @package  Crypt
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  Copyright (c) 2010 Trey Shugart http://europaphp.org/license
+ */
 class Europa_Crypt_Token
 {
+    /**
+     * The data in the token.
+     * 
+     * @var array
+     */
     protected $data = array();
     
+    /**
+     * The shared key.
+     * 
+     * @var string
+     */
     protected $sharedKey;
     
+    /**
+     * Constructs a new token.
+     * 
+     * @param string $shraedKey The shared key to use.
+     * 
+     * @return Europa_Crypt_Token
+     */
     public function __construct($sharedKey)
     {
         $this->sharedKey = $sharedKey;
     }
     
+    /**
+     * Returns the value of a token parameter.
+     * 
+     * @param string $name The parameter name.
+     * 
+     * @return mixed
+     */
     public function __get($name)
     {
         if ($this->__isset($name)) {
@@ -19,17 +51,37 @@ class Europa_Crypt_Token
         return null;
     }
     
+    /**
+     * Sets the value of a token parameter.
+     * 
+     * @param string $name  The parameter name.
+     * @param mixed  $value The parameter value.
+     * 
+     * @return Europa_Crypt_Token
+     */
     public function __set($name, $value)
     {
         $this->data[$name] = $value;
         return $this;
     }
     
+    /**
+     * Checks whether or not the specified token value is set.
+     * 
+     * @param string $name The param name.
+     * 
+     * @return bool
+     */
     public function __isset($name)
     {
         return isset($this->data[$name]);
     }
     
+    /**
+     * Unsets the specified token.
+     * 
+     * @param string $name The param name.
+     */
     public function __unset($name)
     {
         if ($this->__isset($name)) {
@@ -38,11 +90,23 @@ class Europa_Crypt_Token
         return $this;
     }
     
+    /**
+     * Compares the current token to the specified token.
+     * 
+     * @param mixed $token The token to compare.
+     * 
+     * @return bool
+     */
     public function compare($token)
     {
         return $this->__toString() === (string) $token;
     }
     
+    /**
+     * Formats and returns the token as a string.
+     * 
+     * @return string
+     */
     public function __toString()
     {
         return md5($this->sharedKey . serialize($this->data));
