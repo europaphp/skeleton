c62083dba3517bac53d5d723c9a1fdfd88a3c682
Trey Shugart <treshugart@gmail.com>
2010-12-12 09:54:52 +1100

    Changes to the sample controllers for changes in api to Europa_Controller.

--- app/controllers/AbstractController.php
+++ app/controllers/AbstractController.php
@@ -1,7 +1,20 @@
 <?php
 
+/**
+ * An example of controller abstraction that sets up the views.
+ * 
+ * @category Controllers
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  Copyright (c) 2010 Trey Shugart http://europaphp.org/license
+ */
 abstract class AbstractController extends Europa_Controller
 {
+    /**
+     * Sets up the views.
+     * 
+     * @return void
+     */
 	public function init()
 	{
 		$view = str_replace('Controller', 'View', get_class($this));
--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -3,13 +3,20 @@
 /**
  * An example controller sketching some of the features that can be implemented.
  * 
- * @category  Controllers
- * @package   Europa
- * @author    Trey Shugart <treshugart@gmail.com>
- * @copyright (c) 2010 Trey Shugart
- * @link      http://europaphp.org/license
+ * @category Controllers
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  Copyright (c) 2010 Trey Shugart http://europaphp.org/license
  */
 class IndexController extends AbstractController
 {
-	
+    /**
+     * The default request method.
+     * 
+     * @return void
+     */
+	public function get()
+	{
+	    
+	}
 }
