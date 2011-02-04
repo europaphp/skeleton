d162f4d346b2f00807edf18c27bfedd0df3c6bbf
Trey Shugart <treshugart@gmail.com>
2011-02-05 10:34:11 +1100

    Add a call to send a content type header if no headers have been sent yet from the json view.

--- lib/Europa/View/Json.php
+++ lib/Europa/View/Json.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Europa\View;
+
 /**
  * A view class for rendering JSON data from bound parameters.
  * 
@@ -8,30 +10,31 @@
  * @author   Trey Shugart <treshugart@gmail.com>
  * @license  Copyright (c) 2011 Trey Shugart http://europaphp.org/license
  */
-namespace Europa\View
+class Json extends \Europa\View
 {
-    class Json extends \Europa\View
+    /**
+     * Constructs the view and sets parameters.
+     * 
+     * @param mixed $params The parameters to set.
+     * 
+     * @return \Europa\View\Json
+     */
+    public function __construct($params = null)
     {
-        /**
-         * Constructs the view and sets parameters.
-         * 
-         * @param mixed $params The parameters to set.
-         * 
-         * @return \Europa\View\Json
-         */
-        public function __construct($params = null)
-        {
-            $this->setParams($params);
+        $this->setParams($params);
+    }
+    
+    /**
+     * JSON encodes the parameters on the view and returns them.
+     * 
+     * @return string
+     */
+    public function __toString()
+    {
+        // if no headers have been sent, make sure we send the correct mime type
+        if (!headers_sent()) {
+            header('Content-Type: Application/JSON');
         }
-        
-        /**
-         * JSON encodes the parameters on the view and returns them.
-         * 
-         * @return string
-         */
-        public function __toString()
-        {
-            return json_encode($this->getParams());
-        }
+        return json_encode($this->getParams());
     }
 }
