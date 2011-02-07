a4b22f8df0797d8aadad90fe67fef52bfe0b39ca
Trey Shugart <treshugart@gmail.com>
2011-02-07 20:06:47 +1100

    Document.

--- lib/Europa/Reflection/ClassReflector.php
+++ lib/Europa/Reflection/ClassReflector.php
@@ -2,8 +2,21 @@
 
 namespace Europa\Reflection;
 
+/**
+ * Extension to \ReflecitonClass to implement doc block getting/parsing.
+ * 
+ * @category Reflection
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  Copyright (c) 2011 Trey Shugart http://europaphp.org/license
+ */
 class ClassReflector extends \ReflectionClass implements Reflectable
 {
+    /**
+     * Returns the doc block for the class.
+     * 
+     * @return \Europa\Reflection\DocBlock
+     */
     public function getDocBlock()
     {
         return new DocBlock($this->getDocComment());
