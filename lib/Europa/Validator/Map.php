f73c87eb210e2c44526d6a1f3734c62b5af4d406
Trey Shugart <treshugart@gmail.com>
2011-03-03 18:31:38 +1100

    Update validator map.

--- lib/Europa/Validator/Map.php
+++ lib/Europa/Validator/Map.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Europa\Validator;
+
 /**
  * Acts as a validation suite, but maps validators to input data.
  * 
@@ -8,34 +10,50 @@
  * @author   Trey Shugart <treshugart@gmail.com>
  * @license  Copyright (c) 2011 Trey Shugart http://europaphp.org/license
  */
-namespace Europa\Validator
+class Map extends Suite
 {
-    class Map extends Suite
+    /**
+     * Validates the suite based on the attached validators to the passed data.
+     * 
+     * @param mixed $data The data to validate.
+     * 
+     * @return void
+     */
+    public function validate($data)
     {
-        /**
-         * Validates the suite based on the attached validators to the passed data.
-         * 
-         * @param mixed $data The data to validate.
-         * 
-         * @return void
-         */
-        public function validate($data)
-        {
-            // must be an array
-            if (!is_array($data)) {
-                throw new Exception('The data being validated must be an array.');
-            }
-            
-            // reset
-            $this->failed = array();
-            
-            // validate
-            foreach ($this as $id => $validator) {
-                $value = isset($data[$id]) ? $data[$id] : null;
-                $validator->validate($value);
-            }
-            
-            return $this;
+        $this->failed = array();
+        $data         = new ArrayObject($data);
+        $this->preValidate($data);
+        foreach ($this as $id => $validator) {
+            $validator->validate($data[$index]);
         }
+        $this->postValidate($data);
+        return $this;
     }
+
+   /**
+    * Pre-validation hook. Can validate data and modify data before it is passed
+    * to the validators for validation.
+    * 
+    * @param ArrayObject $data The array data to validate and/or modify.
+    * 
+    * @return void
+    */
+   public function preValidate(ArrayObject $data)
+   {
+
+   }
+
+   /**
+    * Post-validation hook. Used for custom validation after validators have been
+    * invoked on the passed in data.
+    * 
+    * @param ArrayObject $data The array data to validate to do any custom validation on.
+    * 
+    * @return void
+    */
+   public function postValidate(ArrayObject $data)
+   {
+
+   }
 }
