749999b163546062e2aaac0916f1d7be7ab6f0e1
unknown <T@.(none)>
2011-03-13 12:59:45 +1100

    Update filters for framework changes.

--- app/filters/AuthFilter.php
+++ app/filters/AuthFilter.php
@@ -1,8 +1,27 @@
 <?php
 
-class AuthFilter extends \Europa\Controller\FilterAbstract
+use Europa\Controller;
+use Europa\Controller\FilterInterface;
+use Europa\String;
+
+/**
+ * Authorization filter for filtering an unathoried user.
+ * 
+ * @category Filters
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  Copyright (c) 2011 Trey Shugart http://europaphp.org/license
+ */
+class AuthFilter implements FilterInterface
 {
-    public function filter()
+	/**
+	 * Filteres the specified controller.
+	 * 
+	 * @param \Europa\Controller $controller The controller to filter.
+	 * 
+	 * @return void
+	 */
+    public function filter(Controller $controller)
     {
         if (!isset($_SESSION['isLoggedIn']) || !$_SESSION['isLoggedIn']) {
             $this->controller->forward('log-in');
