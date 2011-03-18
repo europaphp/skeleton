6bd0ffe41791681473041245c6eea355cc90994f
Trey Shugart <treshugart@gmail.com>
2011-03-18 16:11:15 +1100

    Remove benchmarking class. Unnecessary bloat.

--- www/index.php
+++ www/index.php
@@ -4,15 +4,8 @@
 error_reporting(E_ALL ^ E_STRICT);
 ini_set('display_errors', 'on');
 
-// start benchmark
-require_once('../lib/Europa/Bench.php');
-$bench = new \Europa\Bench;
-
 // load the bootstrap
 require dirname(__FILE__) . '/../app/boot/bootstrap.php';
-
-// so we can view the results in the view
-\Europa\Registry::set('bench', $bench);
 
 // any exceptions will routed to the error controller
 try {
