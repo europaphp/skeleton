c958feb32f801ba34869e704aff6175bbfeea2a1
Trey Shugart <treshugart@gmail.com>
2011-04-04 09:57:36 +1000

    Update cli request to parse parameters again. Turns out \Zend_Console_Getopt and PEAR libraries are failures when it comes to flexibility and configuration :(

--- lib/Europa/Request/Cli.php
+++ lib/Europa/Request/Cli.php
@@ -21,6 +21,25 @@ class Cli extends Request
     const METHOD = 'cli';
     
     /**
+     * Keeps track of the commands that were passed in the request.
+     * 
+     * @var array
+     */
+    private $commands = array();
+    
+    /**
+     * Constructs a cli request and parses out the parameters.
+     * 
+     * @return \Europa\Request\Cli
+     */
+    public function __construct()
+    {
+        $this->parseCommands();
+        $this->parseParams();
+        $this->setController(str_replace(' ', '\\', $this->getCommand()));
+    }
+    
+    /**
      * Returns the request method to call in the controller.
      * 
      * @return string
@@ -28,5 +47,84 @@ class Cli extends Request
     public function method()
     {
         return static::METHOD;
+    }
+    
+    /**
+     * Returns the string command that was passed to the console. The command is the part that occurs just before
+     * the first argument and after the path to the script. This can contain spaces.
+     * 
+     * @param string $imploder The string to implode the commands with.
+     * 
+     * @return string
+     */
+    public function getCommand($imploder = ' ')
+    {
+        return implode($imploder, $this->getCommands());
+    }
+    
+    /**
+     * Returns an array of the commands that were passed in the request.
+     * 
+     * @return array
+     */
+    public function getCommands()
+    {
+        return $this->commands;
+    }
+    
+    /**
+     * Parses out the commands that were used in the request.
+     * 
+     * @return \Europa\Request\Cli
+     */
+    private function parseCommands()
+    {
+        $args = $_SERVER['argv'];
+        $cmds = array();
+        array_shift($args);
+        foreach ($args as $arg) {
+            if (strpos($arg, '-') !== false) {
+                break;
+            }
+            $this->commands[] = $arg;
+        }
+        return $this;
+    }
+    
+    /**
+     * Parses out the cli request parameters - in unix style - and sets them on
+     * the request.
+     * 
+     * @return \Europa\Request\Cli
+     */
+    private function parseParams()
+    {
+        $args = $_SERVER['argv'];
+        $skip = false;
+        array_shift($args);
+        foreach ($args as $index => $param) {
+            if ($skip) {
+                $skip = false;
+                continue;
+            }
+            
+            // allow dash prefixing
+            if ($param[0] === '-') {
+                $cut   = $param[1] === '-' ? 2 : 1;
+                $param = substr($param, $cut, strlen($param));
+                $next  = isset($args[$index + 1]) ? $args[$index + 1] : false;
+            
+                if ($next && $next[0] !== '-') {
+                    $this->setParam($param, $next);
+                    $skip = true;
+                } else {
+                    $this->setParam($param, true);
+                }
+            } else {
+                $this->_commands[] = $param;
+            }
+        }
+        
+        return $this;
     }
 }
