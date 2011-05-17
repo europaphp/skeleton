e373d58fd4fd5f1e3867e6da2a4cc712165598a6
unknown <T@.(none)>
2011-05-17 22:16:26 +1000

    Update FS and FS tests.

--- tests/Test/Fs.php
+++ tests/Test/Fs.php
@@ -1,51 +1,92 @@
 <?php
 
 namespace Test;
-use Europa\Unit\Test\Test;
-use Europa\Fs\File;
 use Europa\Fs\Directory;
+use Europa\Fs\Directory\Exception as DirectoryException;
+use Europa\Fs\File;
+use Europa\Fs\File\Exception as FileException;
+use Europa\Unit\Test\Test;
 
 class Fs extends Test
 {
+    public function init()
+    {
+        if (!$this->testWritable()) {
+            $this->assert(false, "Could not run Europa\Fs tests because the directory {$dir} is not writable.");
+        }
+    }
+
     public function testFileCreateOpenAndDelete()
     {
+        if (!$this->testWritable()) {
+            return;
+        }
+
         try {
-            $file = File::create(dirname(__FILE__) . '/__test.gag');
-        } catch (\Exeception $e) {
-            $this->assert(false, 'The file could not be created.');
+            $file = File::create($this->getTestFile());
+        } catch (FileException $e) {
+            $this->assert(false, "The file could not be created with message: {$e->getMessage()}.");
+            return;
         }
         
         try {
             $file = File::open($file->getPathname());
-        } catch (\Exception $e) {
-            $this->assert(false, 'The file could not be opened. For some reason, it may have not been created, but no exception was thrown.');
+        } catch (FileException $e) {
+            $this->assert(false, "The file could not be opened with message: {$e->getMessage()}.");
+            return;
         }
         
         try {
             $file->delete();
-        } catch (\Exception $e) {
-            $this->assert(false, 'The file could not be deleted. You will have to delete the test file: ' . $file->getPathname());
+        } catch (FileException $e) {
+            $this->assert(false, "The file {$file->getPathname()} could not be deleted with message: {$e->getMessage()}.");
         }
     }
     
     public function testDirectoryCreateOpenAndDelete()
     {
+        if (!$this->testWritable()) {
+            return;
+        }
+
         try {
-            $file = Directory::create(dirname(__FILE__) . '/__test');
-        } catch (\Exeception $e) {
-            $this->assert(false, 'The directory could not be created.');
+            $file = Directory::create($this->getTestDir());
+        } catch (DirectoryException $e) {
+            $this->assert(false, "The directory could not be created with message: {$e->getMessage()}.");
+            return;
         }
         
         try {
             $file = Directory::open($file->getPathname());
-        } catch (\Exception $e) {
-            $this->assert(false, 'The directory could not be opened. For some reason, it may have not been created, but no exception was thrown.');
+        } catch (DirectoryException $e) {
+            $this->assert(false, "The directory could not be opened with message: {$e->getMessage()}.");
+            return;
         }
         
         try {
             $file->delete();
-        } catch (\Exception $e) {
-            $this->assert(false, 'The directory could not be deleted. You will have to delete the test file: ' . $file->getPathname());
+        } catch (DirectoryException $e) {
+            $this->assert(false, "The directory {$file->getPathname()} could not be deleted with message: {$e->getMessage()}.");
         }
+    }
+
+    private function testWritable()
+    {
+        return is_writable($this->getBaseTestDir());
+    }
+
+    private function getTestFile()
+    {
+        return $this->getBaseTestDir() . '/test.file';
+    }
+
+    private function getTestDir()
+    {
+        return $this->getBaseTestDir() . '/test.dir';
+    }
+
+    private function getBaseTestDir()
+    {
+        return realpath(dirname(__FILE__) . '/../../');
     }
 }
