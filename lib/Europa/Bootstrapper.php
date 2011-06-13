02a3a784c25865eea3a4fb8c861349e62e4732e1
Trey Shugart <treshugart@gmail.com>
2011-06-14 09:22:33 +1000

    Add ability to use __invoke to start bootstrapping.

--- lib/Europa/Bootstrapper.php
+++ lib/Europa/Bootstrapper.php
@@ -5,7 +5,7 @@ namespace Europa;
 /**
  * A class used for application setup. Defined methods are called in
  * the order in which they are defined.
- * 
+ *
  * @category Bootstrapper
  * @package  Europa
  * @author   Trey Shugart <treshugart@gmail.com>
@@ -14,20 +14,198 @@ namespace Europa;
 abstract class Bootstrapper
 {
     /**
-     * Goes through each method in the extending class and calls them
-     * in the order in which they were defined.
+     * The default configuration options for all instances.
      * 
-     * @return void
+     * @var array
      */
-    final public function boot()
+    private $options = array();
+    
+    /**
+     * Goes through each method in the extending class and calls them in the order in which they were defined.
+     * 
+     * @return Bootstrapper
+     */
+    final public function __invoke()
     {
         $class = new \ReflectionClass($this);
         foreach ($class->getMethods() as $method) {
-            $name = $method->getName();
-            if ($name === __FUNCTION__ || strpos($name, '__') === 0) {
+            if (!$this->isValidMethod($method)) {
                 continue;
             }
-            $this->$name();
+            $method->invoke($this);
         }
+        return $this;
+    }
+    
+    /**
+     * Sets an option value.
+     * 
+     * @param string $name  The option name.
+     * @param mixed  $value The option value.
+     * 
+     * @return Bootstrapper
+     */
+    final public function __set($name, $value)
+    {
+        return $this->setOption($name, $value);
+    }
+    
+    /**
+     * Returns an option value.
+     * 
+     * @param string $name The option name.
+     * 
+     * @return mixed
+     */
+    final public function __get($name)
+    {
+        return $this->getOption($name);
+    }
+    
+    /**
+     * Checks for the existence of an option.
+     * 
+     * @param string $name The option name.
+     * 
+     * @return Bootstrapper
+     */
+    final public function __isset($name)
+    {
+        return $this->hasOption($name);
+    }
+    
+    /**
+     * Removes an option.
+     * 
+     * @param string $name The option name.
+     * 
+     * @return Bootstrapper
+     */
+    final public function __unset($name)
+    {
+        return $this->removeOption($name);
+    }
+    
+    /**
+     * Sets an option value.
+     * 
+     * @param string $name  The option name.
+     * @param mixed  $value The option value.
+     * 
+     * @return Bootstrapper
+     */
+    final public function setOption($name, $value)
+    {
+        $this->options[$name] = $value;
+        return $this;
+    }
+    
+    /**
+     * Returns an option value.
+     * 
+     * @param string $name The option name.
+     * 
+     * @return mixed
+     */
+    final public function getOption($name)
+    {
+        if ($this->hasOption($name)) {
+            return $this->options[$name];
+        }
+        return null;
+    }
+    
+    /**
+     * Checks for the existence of an option.
+     * 
+     * @param string $name The option name.
+     * 
+     * @return Bootstrapper
+     */
+    final public function hasOption($name)
+    {
+        return isset($this->options[$name]);
+    }
+    
+    /**
+     * Removes an option.
+     * 
+     * @param string $name The option name.
+     * 
+     * @return Bootstrapper
+     */
+    final public function removeOption($name)
+    {
+        if ($this->hasOption($name)) {
+            unset($this->options[$name]);
+        }
+        return $this;
+    }
+    
+    /**
+     * Sets the default configuration for the bootstrapper. Uses late static binding so that different objects can have
+     * separate default configurations.
+     * 
+     * @param array $options The options to set for this type of object.
+     * 
+     * @return Bootstrapper
+     */
+    final public function setOptions($options)
+    {
+        if (is_object($options) || is_array($options)) {
+            foreach ($options as $name => $value) {
+                $this->setOption($name, $value);
+            }
+        }
+        return $this;
+    }
+    
+    /**
+     * Returns the default options.
+     * 
+     * @return array
+     */
+    final public function getOptions()
+    {
+        return $this->defaultOptions;
+    }
+    
+    /**
+     * Removes all options.
+     * 
+     * @return Bootstrapper
+     */
+    final public function removeOptions()
+    {
+        $this->options = array();
+        return $this;
+    }
+    
+    /**
+     * Returns whether or not the method is a valid bootstrap method.
+     * 
+     * @param \ReflectionMethod $method The method to check.
+     * 
+     * @return bool
+     */
+    private function isValidMethod(\ReflectionMethod $method)
+    {
+        if ($method->isConstructor()) {
+            return false;
+        }
+        
+        if (!$method->isPublic()) {
+            return false;
+        }
+        
+        if ($method->class !== get_class($this)) {
+            return false;
+        }
+        
+        if (strpos($method->getName(), '__') === 0) {
+            return false;
+        }
+        
+        return true;
     }
 }
