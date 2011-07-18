04870828aac5ff4f1c9389d0197905c1d88eaed3
Trey Shugart <treshugart@gmail.com>
2011-07-18 22:51:26 +1000

    Create a dispatcher interface and make the generic dispatcher implement it. Moved request controller manipulation to the dispatcher.

---
+++ lib/Europa/Dispatcher/DispatcherInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Europa\Dispatcher;
+use Europa\Request\RequestInterface;
+use Europa\Response\ResponseInterface;
+
+/**
+ * The most basic version of a dispatcher.
+ *
+ * @category Controller
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  Copyright (c) 2011 Trey Shugart http://europaphp.org/license
+ */
+interface DispatcherInterface
+{
+    /**
+     * Renders, sends all headers and outputs the result.
+     * 
+     * @return void
+     */
+    public function dispatch(RequestInterface $request, ResponseInterface $response);
+}
---
+++ lib/Europa/Dispatcher/Exception.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Europa\Dispatcher;
+use Europa\Exception as Base;
+
+class Exception extends Base
+{
+    
+}
