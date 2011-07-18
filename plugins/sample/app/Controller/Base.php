e7595aee96c868dcfa647bdcfb493ebae5ec4400
Trey Shugart <treshugart@gmail.com>
2011-07-18 22:52:06 +1000

    Update sample app files to accomodate changes.

--- plugins/sample/app/Controller/Base.php
+++ plugins/sample/app/Controller/Base.php
@@ -1,27 +1,14 @@
 <?php
 
 namespace Controller;
-use Europa\Controller\ControllerAbstract;
+use Europa\Controller\Rest;
 use Europa\Di\Container;
 
-/**
- * An example of controller abstraction that sets up a default view scheme.
- * 
- * @category Controllers
- * @package  Europa
- * @author   Trey Shugart <treshugart@gmail.com>
- * @license  Copyright (c) 2011 Trey Shugart http://europaphp.org/license
- */
-abstract class Base extends ControllerAbstract
+abstract class Base extends Rest
 {
-    /**
-     * Sets up the a default view scheme.
-     * 
-     * @return void
-     */
     public function init()
     {
-        $view = Container::get()->phpView->get();
+        $view = Container::get()->view->get();
         $view->setScript(get_class($this));
         $this->setView($view);
     }
