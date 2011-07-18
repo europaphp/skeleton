b07f71965afdf8e5d83400409f501bc528143c51
Trey Shugart <treshugart@gmail.com>
2011-07-18 22:51:45 +1000

    Update view interfaces for generic views and scriptable views.

--- lib/Europa/View/ViewScriptInterface.php
+++ lib/Europa/View/ViewScriptInterface.php
@@ -2,9 +2,29 @@
 
 namespace Europa\View;
 
+/**
+ * Allows a script to render files.
+ * 
+ * @category Views
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  Copyright (c) 2011 Trey Shugart http://europaphp.org/license
+ */
 interface ViewScriptInterface extends ViewInterface
 {
+    /**
+     * Sets the script to be rendered.
+     * 
+     * @param string $script The script to render.
+     * 
+     * @return ViewScriptInterface
+     */
     public function setScript($script);
     
+    /**
+     * Returns the script to be rendered.
+     * 
+     * @return string
+     */
     public function getScript();
 }
