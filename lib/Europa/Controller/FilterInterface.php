99321aa0733dce6526472842936d0d34efb6440c
Trey Shugart <treshugart@gmail.com>
2011-07-18 22:49:32 +1000

    Further abstract controller functionality.
    Create sub-abstract-class of ControllerAbstract called Rest for restful
    controllers. Create a controller interface so that custom-controller imp
    lementations can be created and passed around.

--- lib/Europa/Controller/FilterInterface.php
+++ lib/Europa/Controller/FilterInterface.php
@@ -1,9 +1,21 @@
 <?php
 
 namespace Europa\Controller;
-use Europa\Controller;
 
+/**
+ * The most basic filter implementation allowed for controllers.
+ *
+ * @category Controller
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  Copyright (c) 2011 Trey Shugart http://europaphp.org/license
+ */
 interface FilterInterface
 {
-    public function filter(Controller $controller);
+    /**
+     * Filters the specified controller.
+     * 
+     * @return void
+     */
+    public function filter(ControllerInterface $controller);
 }
