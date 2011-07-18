7031ea8ae30bb282070f7d581a9c6bddc03d04ff
Trey Shugart <treshugart@gmail.com>
2011-07-18 22:51:45 +1000

    Update view interfaces for generic views and scriptable views.

--- lib/Europa/View/ViewInterface.php
+++ lib/Europa/View/ViewInterface.php
@@ -2,7 +2,22 @@
 
 namespace Europa\View;
 
+/**
+ * Most basic view implementation.
+ * 
+ * @category Views
+ * @package  Europa
+ * @author   Trey Shugart <treshugart@gmail.com>
+ * @license  Copyright (c) 2011 Trey Shugart http://europaphp.org/license
+ */
 interface ViewInterface
 {
+    /**
+     * Renders the specified view using the specified context.
+     * 
+     * @param array $context The context to render.
+     * 
+     * @return string
+     */
     public function render(array $context = array());
 }
