7ca6541074c6162fde9a887eec1670563f5da731
Trey Shugart <treshugart@gmail.com>
2011-11-07 23:47:50 +1100

    Test bootstrapping modifications.

--- bin/tests.php
+++ bin/tests.php
@@ -1,19 +1,26 @@
 <?php
 
 use Europa\Application\Container;
+use Europa\Fs\Loader;
+use Europa\Fs\Locator\PathLocator;
 use Testes\Output\Cli as Output;
 use Test as Test;
 
-// base path to install
+// base path for install
 $base = dirname(__FILE__) . '/../';
 
-// all we need to do is include the bootstrap
+// make sure we include application configuration
 require $base . 'app/Boot/bootstrap.php';
 
 // add the tests path
-$locator = Container::get()->loaderLocator->get();
+$locator = new PathLocator;
 $locator->addPath($base . 'vendor/Testes/lib');
 $locator->addPath($base . 'tests');
+
+// register loading
+$loader = new Loader;
+$loader->setLocator($locator);
+$loader->register();
 
 // run the tests
 $tests = new Test;
