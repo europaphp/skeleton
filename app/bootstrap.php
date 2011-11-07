c6292d5c7d9f32baef5855129e847bca0bb99d54
Trey Shugart <treshugart@gmail.com>
2011-11-08 00:46:21 +1100

    Just use basic config rather than an array.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,7 +5,6 @@ ini_set('display_errors', 'on');
 error_reporting(E_ALL | E_STRICT);
 
 use Europa\Application\Configurator\Basic;
-use Europa\Application\ConfiguratorArray;
 use Europa\Application\Container;
 use Europa\Fs\Loader;
 
@@ -15,8 +14,7 @@ $loader = new Loader;
 $loader->register();
 
 // uses default configuration, but you can also specify your own
-$config = new ConfiguratorArray;
-$config->add(new Basic);
+$config = new Basic;
 $config->configure(Container::get());
 
 // kick off autoloading for everything else
