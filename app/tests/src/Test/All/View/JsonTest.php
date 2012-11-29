e0288ca80577a22d62401252190d9021ad8fcadb
Trey Shugart <treshugart@gmail.com>
2012-11-29 14:07:55 +1100

    Rename JSON test.

--- app/tests/src/Test/All/View/JsonTest.php
+++ app/tests/src/Test/All/View/JsonTest.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace Test\All\View;
-use Europa\View\Json as JsonView;
+use Europa\View\Json;
 use Testes\Test\UnitAbstract;
 
-class Json extends UnitAbstract
+class JsonTest extends UnitAbstract
 {
     function rendering()
     {
-        $view = new JsonView;
+        $view = new Json;
         $data = array(
             'data' => array(
                 'val1' => 1,
