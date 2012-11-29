8e17ba9077b63cb3e8daa45901c7a3e4f97c848a
Trey Shugart <treshugart@gmail.com>
2012-11-29 14:07:17 +1100

    Update ToStringFilter to serialize objects not just arrays.

--- src/Europa/Filter/ToStringFilter.php
+++ src/Europa/Filter/ToStringFilter.php
@@ -21,13 +21,17 @@ class ToStringFilter
      */
     public function __invoke($value)
     {
+        if (is_string($value)) {
+            return $value;
+        }
+
         if ($value === true) {
             $value = 'true';
         } elseif ($value === false) {
             $value = 'false';
         } elseif (is_numeric($value)) {
             $value = (string) $value;
-        } elseif (is_array($value)) {
+        } elseif (is_array($value) || is_object($value)) {
             $value = serialize($value);
         } elseif (is_null($value)) {
             $value = 'null';
