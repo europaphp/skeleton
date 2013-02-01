14f91c0e379855e423f4f7c254deeceb2558847f
Trey Shugart <treshugart@gmail.com>
2013-02-01 11:58:13 +1100

    Moving of modules into separate repositories.

--- app/europaphp/main/views/cli.php
+++
@@ -1,9 +0,0 @@
-
-<?php echo $this->helper('cli')->color('EuropaPHP Command Line Interface', 'cyan'); ?>
-
-<?php echo $this->helper('cli')->color('--------------------------------', 'cyan'); ?>
-
-
-<?php echo trim($this->renderChild()); ?>
-
-
