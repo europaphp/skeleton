de0ef6120ac5178c02fa89c4ad5b7a160a762064
Trey Shugart <treshugart@gmail.com>
2013-09-30 23:05:03 +1000

    Get skeleton working.

--- www/index.php
+++ www/index.php
@@ -5,11 +5,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $container = new Europaphp\Main\Di\Container;
 $container('loader')->register();
 $container('modules')
-    ->add(new Europaphp\Help)
-    ->add(new Europaphp\Main)
-    ->add(new Europaphp\Tests([
-        'global-analysis-paths' => [__DIR__ . '/../src']
-    ]));
+  ->add(new Europaphp\Help)
+  ->add(new Europaphp\Main);
 
 $app = $container('app');
 $app();
